c6b9188253e1186da9dda2bcc6edb87648232400
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-02-23 14:01:46 +0300

    Update pytubetest2.py

--- pytubetest2.py
+++ pytubetest2.py
@@ -3,7 +3,7 @@ video_url = input("Enter a YouTube video URL: ")
 try:
     yt = YouTube(video_url)
     audio_stream = yt.streams.filter(only_audio=True).first()
-    save_directory = r'C:\Users\bulug\Desktop\pytubetest'
+    save_directory = r'\Desktop'
     audio_stream.download(output_path=save_directory, filename=f"{yt.title}.mp3")
     print("Audio downloaded successfully!")
 except Exception as e:
