406784e1090ddbdec9dddd039c3747a7770a4b0d
Victiniiiii <138769411+Victiniiiii@users.noreply.github.com>
2024-06-27 02:06:28 +0300

    automatically finds your desktop folder

--- pytubetest2.py
+++ pytubetest2.py
@@ -1,10 +1,11 @@
+import os
 from pytube import YouTube
 video_url = input("Enter a YouTube video URL: ")
 try:
     yt = YouTube(video_url)
     audio_stream = yt.streams.filter(only_audio=True).first()
-    save_directory = r'\Desktop'
-    audio_stream.download(output_path=save_directory, filename=f"{yt.title}.mp3")
+    user_desktop = os.path.join(os.path.expanduser("~"), "Desktop")    
+    audio_stream.download(output_path=user_desktop, filename=f"{yt.title}.mp3")
     print("Audio downloaded successfully!")
 except Exception as e:
     print(f"An error occurred: {str(e)}")
